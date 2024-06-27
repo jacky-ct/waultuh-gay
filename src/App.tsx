662254bd7727c3c580a023ac4e33843798992dab
Jackson Thomas <jacksonchristopherthomas@gmail.com>
2024-06-27 20:40:49 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <span class="mario">
+          <b>MARIO UPSMASH</b>
+        </span>
+      </div>
+      <div>
+        <img src="../public/upsmash.webp" />
+      </div>
+    </>
+  );
+}
+
+export default App;
